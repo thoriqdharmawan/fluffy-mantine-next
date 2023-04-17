495557385ae473a82713e06627e0aaaad0af1932
thoriqdharmawan <thoriqd21@gmail.com>
2023-04-17 14:12:22 +0700

    feat: add incomes section

--- mock/global.ts
+++ mock/global.ts
@@ -1,3 +1,5 @@
+import dayjs from "dayjs";
+
 export interface GLOBAL_SELECT_TYPE {
   value: string;
   label: string;
@@ -22,3 +24,33 @@ export const DEFAULT_CATEGORY = [
     label: 'Pakaian',
   },
 ];
+
+
+interface VariablesDate {
+  [key: string]: {
+    startdate: string,
+    enddate: string
+  }
+}
+
+const Now = dayjs(new Date())
+const FormatDate = 'YYYY-MM-DD'
+
+export const VARIABLES_DATE: VariablesDate = {
+  'NOW': {
+    startdate: Now.format(FormatDate),
+    enddate: Now.add(1, 'day').format(FormatDate)
+  },
+  'YESTERDAY': {
+    startdate: Now.subtract(1, 'day').format(FormatDate),
+    enddate: Now.format(FormatDate)
+  },
+  'THISMONTH': {
+    startdate: Now.startOf('month').format(FormatDate),
+    enddate: Now.endOf('month').format(FormatDate)
+  },
+  'LAST30DAYS': {
+    startdate: Now.subtract(30, 'days').format(FormatDate),
+    enddate: Now.format(FormatDate)
+  },
+}
