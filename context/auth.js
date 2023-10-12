d0aac9ffc3c7e379b848862e74e00bd1a4f7568a
thoriqdharmawan <thoriqd21@gmail.com>
2023-10-12 10:24:59 +0700

    fix: login condition

--- context/auth.js
+++ context/auth.js
@@ -1,35 +1,39 @@
-import { useEffect } from 'react'
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
-import { InitialUserState, useUser } from './user'
-import { Authentication } from '../services/authentication'
+import { InitialUserState, useUser } from './user';
+import { Authentication } from '../services/authentication';
 
 const AuthStateChangeProvider = ({ children }) => {
-  const user = useUser()
-  const { SetUser } = user
+  const user = useUser();
+  const router = useRouter();
+  const { SetUser } = user;
 
   const InitiateAuthStateChange = () => {
     Authentication().onAuthStateChanged((user) => {
       if (user) {
-        console.log('User is authenticated')
+        console.log('User is authenticated');
         SetUser({
           uid: user.uid,
           email: user.email,
-          displayName: user.displayName || "Admin",
+          displayName: user.displayName || 'Admin',
           photoURL: user.photoURL,
-          emailVerified: user.emailVerified
-        })
+          emailVerified: user.emailVerified,
+        });
       } else {
-        console.log('User is not authenticated')
-        SetUser(InitialUserState)
+        console.log('User is not authenticated');
+
+        router.push('/login');
+        SetUser(InitialUserState);
       }
-    })
-  }
+    });
+  };
 
   useEffect(() => {
-    InitiateAuthStateChange()
-  }, [])
+    InitiateAuthStateChange();
+  }, []);
 
-  return children
-}
+  return children;
+};
 
-export default AuthStateChangeProvider
+export default AuthStateChangeProvider;
--- context/user.js
+++ context/user.js
@@ -1,56 +1,62 @@
-import { createContext, useContext, useState, useEffect } from 'react'
-import client from '../apollo-client'
-import { GET_LIST_USER_LOGIN } from '../services/users'
+import { createContext, useContext, useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { GET_LIST_USER_LOGIN } from '../services/users';
+
+import client from '../apollo-client';
 
 export const InitialUserState = {
   uid: null,
   email: null,
   displayName: null,
   photoURL: null,
-  emailVerified: false
-}
+  emailVerified: false,
+};
 
-const UserContext = createContext()
+const UserContext = createContext();
 
 const getDataUser = async (uid) => {
   const { data } = await client.query({
     query: GET_LIST_USER_LOGIN,
     variables: {
-      uid
-    }
-  })
+      uid,
+    },
+  });
 
-  return data
-}
+  return data;
+};
 
 export const useUser = () => {
-  return useContext(UserContext)
-}
+  return useContext(UserContext);
+};
 
 export const UserProvider = (props) => {
-  const [userState, setUserState] = useState(InitialUserState)
+  const [userState, setUserState] = useState(InitialUserState);
+  const router = useRouter();
 
   useEffect(() => {
     if (userState.uid) {
-      getDataUser(userState.uid).then(({ users }) => {
-        setUserState((prev) => ({
-          ...prev,
-          displayName: users?.[0].name,
-          companyId: users?.[0].companyId
-        }))
-      })
+      getDataUser(userState.uid)
+        .then(({ users }) => {
+          setUserState((prev) => ({
+            ...prev,
+            displayName: users?.[0].name,
+            companyId: users?.[0].companyId,
+          }));
+        })
+        .catch(() => {
+          router.push('/login');
+        });
     }
-  }, [userState.uid])
-
+  }, [userState.uid]);
 
   const SetUser = (userCredential) => {
-    setUserState({ ...userCredential })
-  }
+    setUserState({ ...userCredential });
+  };
 
   const ResetUser = () => {
-    setUserState(InitialUserState)
-  }
+    setUserState(InitialUserState);
+  };
 
-  const value = { ...userState, SetUser, ResetUser }
-  return <UserContext.Provider value={value} {...props} />
-}
+  const value = { ...userState, SetUser, ResetUser };
+  return <UserContext.Provider value={value} {...props} />;
+};
