0742b36fd7e584e2b35d936a16e247b0008fe319
thoriqdharmawan <thoriqd21@gmail.com>
2023-12-29 21:34:12 +0700

    feat: implement login style

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -1,7 +1,8 @@
-import { TextInput, Checkbox, Button, Group, Box } from '@mantine/core';
+import { Box, Paper, TextInput, PasswordInput, Button, Title, Text, Anchor } from '@mantine/core';
+import { useRouter } from 'next/router';
 import { useForm } from '@mantine/form';
 import { SignIn } from '../../services/authentication';
-import { useRouter } from 'next/router';
+import { ColorSchemeToggle } from '../../components/color-scheme-toggle';
 
 type formLoginType = {
   email: string;
@@ -34,26 +35,69 @@ export default function index() {
   };
 
   return (
-    <Box sx={{ maxWidth: 300 }} mt="xl" mx="auto">
-      <form onSubmit={form.onSubmit(handleSubmit)}>
-        <TextInput
-          withAsterisk
-          label="Email"
-          placeholder="Masukan Email"
-          {...form.getInputProps('email')}
-        />
-        <TextInput
-          withAsterisk
-          label="Password"
-          type="password"
-          placeholder="Masukan Password"
-          {...form.getInputProps('password')}
-        />
-
-        <Group position="right" mt="md">
-          <Button type="submit">Login</Button>
-        </Group>
-      </form>
+    <Box sx={styles.wrapper as any}>
+      <Paper sx={styles.form as any} radius={0} p={30}>
+        <Box>
+          <Title order={3} ta="center" mt="md" mb={50}>
+            Selamat Datang di Fluffy Pos
+          </Title>
+          <form onSubmit={form.onSubmit(handleSubmit)}>
+            <TextInput
+              label="Email"
+              placeholder="email@gmail.com"
+              size="md"
+              {...form.getInputProps('email')}
+            />
+            <PasswordInput
+              label="Password"
+              placeholder="Masukan Password"
+              mt="md"
+              size="md"
+              {...form.getInputProps('password')}
+            />
+            <Button type="submit" fullWidth mt="xl" size="md">
+              Login
+            </Button>
+          </form>
+        </Box>
+        <Text fz="xs" ta="center">
+          Dibuat oleh{' '}
+          <Anchor href="https://www.thoriq.pro/" target="_blank">
+            https://www.thoriq.pro/
+          </Anchor>
+        </Text>
+      </Paper>
+      <Box sx={styles.fab as any}>
+        <ColorSchemeToggle />
+      </Box>
     </Box>
   );
 }
+
+const styles = {
+  wrapper: {
+    minHeight: '550px',
+    height: '100vh',
+    backgroundSize: 'cover',
+    position: 'relative',
+    backgroundImage:
+      'url(https://images.unsplash.com/photo-1484242857719-4b9144542727?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1280&q=80)',
+  },
+  form: {
+    minHeight: '550px',
+    maxWidth: '450px',
+    paddingTop: '80px',
+    height: '100vh',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    '@media (max-width: 768px)': {
+      maxWidth: '100%',
+    },
+  },
+  fab: {
+    position: 'absolute',
+    top: 22,
+    right: 22,
+  },
+};
